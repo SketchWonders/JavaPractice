96b7310bbc20a88f3a2868453394a4506d61ceb4
Ashley Wonders <ADW79@pitt.edu>
2022-02-07 22:53:18 -0500

    First program Hello World

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+//Welcome.java
+
+public class Welcome{
+	public static void main(String[] args){
+		//Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+		System.out.println("Hello World!");
+	}
+}
